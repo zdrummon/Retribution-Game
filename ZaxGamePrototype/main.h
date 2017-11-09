86117947c8680b22007d6ce0645a89a698706565
Zachary Drummond <zdrummon@gmail.com>
2017-11-08 22:22:19 -0800

    Minor text tweak an added a makeless readme

--- ZaxGamePrototype/main.h
+++ ZaxGamePrototype/main.h
@@ -28,7 +28,7 @@ bool initSDL() {
     } else {
         
 		//Create window
-        gWindow = SDL_CreateWindow ("makefile compile test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        gWindow = SDL_CreateWindow ("SDL WINDOW TUTORIAL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		
 		if (gWindow == NULL) {
 			
