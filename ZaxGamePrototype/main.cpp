c0504bc2db9e441a5a9afea99b37c8581e314a00
Trevor Haggerty <cesismalon@gmail.com>
2017-11-15 03:58:16 -0800

    Fixed broken code, optimized, and refactored

--- ZaxGamePrototype/main.cpp
+++ ZaxGamePrototype/main.cpp
@@ -6,57 +6,47 @@
 #include <string>
 //headers
 #include "main.h"
-#include "StartGame.h"
-#include "RunGame.h"
-#include "Exitgame.h"
 //namespaces
 using namespace std;
-
 //main class body
 int main (int argc, char* args[]) {
-	
+
 	//{---LOG---start debug log
 	startTicker = clock();
 	logCount = 0;
-	printf ("\n START DEBUG LOG\n =============================="); //}
+	printf ("\n DEBUG LOG\n =============================="); //}
 	
-	//Starts up SDL and creates window 
-	if (!initSDL()) {
+	if (!initSDLHandler()) {
 		
-		//{---LOG---failed to initialize SDL	
+		//{---LOG---failed to initialize SDL
 		logEvents();
 		printf ("failed to initialize SDL!"); //}
 	
 	} else {
 		
-		//Loads media
-		if (!loadMedia()) {
+		if (!mediaHandler()) {
 			
-			//{---LOG---failed to load media	
+			//{---LOG---failed to load media
 			logEvents();
 			printf ("failed to load media!"); //}
 			
 		} else {
 			
-			//Main loop flag
-            bool quit = false;
+            bool quitFlag = false;
             
-			//Event handler
-            SDL_Event e;
-			
-			//Set default current surface
-            gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_DEFAULT];
-						
-			//While application is running
-            while (!quit) {
+            SDL_Event eventHandler;	
+
+			currentSurface = keySurfaces [KEY_PRESS_DEFAULT];			
+	
+            while (!quitFlag) {
 				
 				//Handle events on queue
-                while (SDL_PollEvent (&e) != 0) {
+                while (SDL_PollEvent (&eventHandler) != 0) {
 					
                     //User requests quit
-                    if (e.type == SDL_QUIT) {
+                    if (eventHandler.type == SDL_QUIT) {
 						
-						quit = true;	
+						quitFlag = true;	
 						
 						//{---LOG---quit flag has been set
 						logEvents();
@@ -64,84 +54,62 @@ int main (int argc, char* args[]) {
                     }
 					
 					//User presses a key
-                    if (e.type == SDL_KEYDOWN) {
+                    if (eventHandler.type == SDL_KEYDOWN) {
 						
 						//Select surfaces based on key press
-                        switch (e.key.keysym.sym) {
+                        switch (eventHandler.key.keysym.sym) {
                             
 							case SDLK_UP:
 							//{---LOG---up key pressed
 							logEvents();
 							printf ("up key pressed"); //}
-                            gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_UP];
+							
+                            currentSurface = keySurfaces [KEY_PRESS_UP];
                             break;
 
                             case SDLK_DOWN:
 							//{---LOG---down key pressed
 							logEvents();
-							printf ("down key pressed"); //}							
-                            gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_DOWN];
+							printf ("down key pressed"); //}	
+							
+                            currentSurface = keySurfaces [KEY_PRESS_DOWN];
                             break;
 
                             case SDLK_LEFT:
 							//{---LOG---left key pressed
 							logEvents();
-							printf ("left key pressed"); //}							
-                            gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_LEFT];
+							printf ("left key pressed"); //}	
+							
+                            currentSurface = keySurfaces [KEY_PRESS_LEFT];
                             break;
 
                             case SDLK_RIGHT:
 							//{---LOG---right key pressed
 							logEvents();
-							printf ("right key pressed"); //}							
-                            gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_RIGHT];
+							printf ("right key pressed"); //}	
+							
+                            currentSurface = keySurfaces [KEY_PRESS_RIGHT];
                             break;
 
                             default:						
-                            gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_DEFAULT];
+                            currentSurface = keySurfaces [KEY_PRESS_DEFAULT];
                             break;
                         }
                     }
                 }
 				
-				//Apply the image stretched
-				SDL_Rect stretchRect;
-				stretchRect.x = 0;
-				stretchRect.y = 0;
-				stretchRect.w = SCREEN_WIDTH;
-				stretchRect.h = SCREEN_HEIGHT;
-				SDL_BlitScaled (gStretchedSurface, NULL, gScreenSurface, &stretchRect);
-			
-				//Update the surface
-				SDL_UpdateWindowSurface (gWindow);
-			
+				//Apply the current image
+                SDL_BlitSurface (currentSurface, NULL, windowSurface, NULL);
+				SDL_UpdateWindowSurface (windowHandler);			
 			}
 		}
 	}
-		
-//{ OLD PROGRAM
-//	//start the game
-//	StartGame StartGameObject;
-//	StartGameObject.startScreen(0,0,0,1);
-//	StartGameObject.startMenu();
-//	
-//	//run the game
-//	if (StartGameObject.startMenu() == 1) {	
-//		RunGame RunGameObject;
-//		RunGameObject.playGame();
-//	}
-//	
-//	//exit the game
-//	ExitGame ExitGameObject;
-//	ExitGameObject.exitScreen();
-//}
-	
-	//Frees media and shuts down SDL
-	void closeSDL();
+
+	void closeSDLHandler();
 	
 	//{---LOG---program has ended
 	logEvents();
-	printf ("program has ended\n ==============================\n END DEBUG LOG\n"); //}
+	printf ("program has ended\n ==============================\n"); //}
 
-	return 0;	
+	return 0;			
 }
