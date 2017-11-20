b1089554459a7c18d7b8bc05271a62cdfdedc1be
Trevor Haggerty <cesismalon@gmail.com>
2017-11-19 20:35:19 -0800

    partially refactored graphics handler

--- source/GraphicsHandler.cpp
+++ source/GraphicsHandler.cpp
@@ -6,38 +6,7 @@
 //headers
 #include "LogHandler.h"
 
-	
-const int MAP_WIDTH_SET = 10;
-const int MAP_HEIGHT_SET = 10;
-const int TILE_WIDTH = 32;
-const int TILE_HEIGHT = 32;
-const int SCREEN_WIDTH = TILE_WIDTH * MAP_WIDTH;
-const int SCREEN_HEIGHT = TILE_HEIGHT * MAP_HEIGHT;
-
-enum MapTile {
-    MAP_TILE_DEFAULT,
-    MAP_TILE_WALL,
-    MAP_TILE_FLOOR,
-	MAP_TILE_PLAYER,
-	MAP_TILE_ENEMY,
-    MAP_TILE_TOTAL
-};
-
-//{ objects/pointers
-//Loads individual image
-SDL_Surface* loadSurface (std::string path);
-
-//The window we'll be rendering to
-SDL_Window* windowHandler = NULL;
-    
-//The surface contained by the window
-SDL_Surface* windowSurface = NULL;
-
-//The images that correspond to a keypress
-SDL_Surface* mapSurfaces [MAP_TILE_TOTAL];
-
-//Current displayed image
-SDL_Surface* currentSurface = NULL; //}	
+GraphicsHandler::GraphicsHandler() {}	
 	
 bool initSDLHandler() {
 	
--- source/headers/GraphicsHandler.h
+++ source/headers/GraphicsHandler.h
@@ -1,19 +1,55 @@
 #pragma once
 
-namespace GraphicsHandler {
+class LogHandler {
 	
-	extern int MAP_WIDTH_SET;
-	extern int MAP_HEIGHT_SET;
-	extern int TILE_WIDTH;
-	extern int TILE_HEIGHT;
-	extern int SCREEN_WIDTH;
-	extern int SCREEN_HEIGHT;
+	public:
+		GraphicsHandler();
 		
-	bool initSDLHandler();
-	bool mediaHandler();
-	void closeSDLHandler();
-	//SDL_Surface* loadSurface();
-	void mapGraphicsPrinter();
+		bool initializeSDLHandler();
+		bool mediaLoader();
+		SDL_Surface* loadSurface (std::string path);
+		void closeSDLHandler();
+		void mapGraphicsPrinter();
 
-}
-
+		int getMAP_WIDTH();
+		int getMAP_HEIGHT();
+		int getTILE_WIDTH();
+		int getTILE_HEIGHT();
+		int getSCREEN_WIDTH();
+		int getSCREEN_HEIGHT();
+		int getMapTile();
+		
+	protected:	
+	
+	private:
+		const int MAP_WIDTH = 10;
+		const int MAP_HEIGHT = 10;
+		const int TILE_WIDTH = 32;
+		const int TILE_HEIGHT = 32;
+		const int SCREEN_WIDTH = TILE_WIDTH * MAP_WIDTH;
+		const int SCREEN_HEIGHT = TILE_HEIGHT * MAP_HEIGHT;
+		
+		enum MapTile {
+			MAP_TILE_DEFAULT,
+			MAP_TILE_WALL,
+			MAP_TILE_FLOOR,
+			MAP_TILE_PLAYER,
+			MAP_TILE_ENEMY,
+			MAP_TILE_TOTAL
+		};
+		
+		//Loads individual image
+		SDL_Surface* loadSurface (std::string path);
+		
+		//The window we'll be rendering to
+		SDL_Window* windowHandler = NULL;
+			
+		//The surface contained by the window
+		SDL_Surface* windowSurface = NULL;
+		
+		//The images that correspond to a keypress
+		SDL_Surface* mapSurfaces [MAP_TILE_TOTAL];
+		
+		//Current displayed image
+		SDL_Surface* currentSurface = NULL;
+};
